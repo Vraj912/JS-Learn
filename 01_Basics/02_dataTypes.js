74a2f53b3fa031ca5c8c0eaef4a59edddf0cf13f
Vraj Patel <92243237+Vraj912@users.noreply.github.com>
2024-06-05 18:15:21 +0000

    changes made

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+console.log("Vraj");
+
+let name = "Vraj"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+
+// object
+
+console.log(typeof "Vraj");
+console.log(typeof 18);
