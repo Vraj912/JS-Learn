28a6e4607350abc6cfdd8e015d1a1c7a41cef6e2
Vraj Patel <92243237+Vraj912@users.noreply.github.com>
2024-06-05 18:02:45 +0000

    Some code with investigation model and basics.

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,17 @@
-console.log("Vraj");
+const accountId = 144553
+let accountEmail = "vraj@google.com"
+var accountPassword = "12345"
+accountCity = "Amdavad"
+let accountState
+// accountId = 2 // not allowed
+
+accountEmail = "vp@vp.com"
+accountPassword = "21212121"
+accountCity = "Mumbai"
+
+/**
+ * This is also one kind of comment.
+ */
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
